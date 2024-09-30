5f7d84778b6541d1a1ac9b69e22f6a62d45da5e7
Manvi Garg <91490442+Manvi008@users.noreply.github.com>
2024-09-30 23:40:20 +0530

    Create FindingAsciiValueusingClass.cpp

---
+++ FindingAsciiValueusingClass.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class ASCII{
+    public :
+    void value(){
+       char c ='A' ;
+       cout<<"The ASCII value of A is"<<" "<<int(c);
+    }
+};
+int main(){
+    ASCII p1;
+    p1.value();
+    return 0 ;
+}
